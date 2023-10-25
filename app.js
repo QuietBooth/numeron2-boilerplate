5126ab6ce2c2b995be48a602cc80b6dfa7786922
Nivaash <nidishnivash@gmail.com>
2023-10-26 00:13:00 +0530

    done

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 // Iteration 1: Making the play button in the index.html functional.
 // Description: When the play button is clicked the game.html page should be opened
+
+
+play_button = document.getElementById('play-button');
+
+play_button.onclick = () =>{
+    window.location.href = ('game.html');
+}
--- score.js
+++ score.js
@@ -1,1 +1,13 @@
 // Iteration 8: Making scoreboard functional
+
+let ply_btn = document.getElementById("play-again-button")
+
+ply_btn.addEventListener("click",redirect)
+
+function redirect(){
+    location.href="./game.html"
+}
+
+let score__ = document.getElementById('score-board');
+let l_storage = localStorage.getItem("score");
+score__.innerHTML = l_storage;
